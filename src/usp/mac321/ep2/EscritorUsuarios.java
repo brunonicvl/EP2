f09adbfb87fe2928f9432121ca19063d68818612
brunonicvl <brunonicladosky@gmail.com>
2024-06-25 19:38:41 -0300

    Create EscritorUsuarios.java

---
+++ src/usp/mac321/ep2/EscritorUsuarios.java
@@ -0,0 +1,23 @@
+package usp.mac321.ep2;
+
+import java.io.*;
+import java.util.*;
+
+public class EscritorUsuarios implements EscritorUsuariosDAO {
+
+	@Override
+	public void escreveUsuarios(List<Usuario> l, String nomeArquivoUsuarios) {
+		try {
+			PrintWriter writer = new PrintWriter(nomeArquivoUsuarios, "UTF-8");
+			writer.println("Apelido,Nome");
+			for(Usuario i: l) {
+				writer.println(i.getApelido() + "," + i.getNome());
+			}
+			writer.close();
+		}
+		catch(FileNotFoundException | UnsupportedEncodingException e) {
+			System.out.println(e);
+		} 
+	}
+
+}
